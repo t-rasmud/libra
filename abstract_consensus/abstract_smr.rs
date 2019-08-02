0791bf1a1219aa0c51552e0a948215ca2447336d
Rashmi Mudduluru <rashmimudduluru@calibra.com>
2019-08-02 13:40:00 -0700

    blockstore APIs

--- abstract_consensus/abstract_smr.rs
+++ abstract_consensus/abstract_smr.rs
@@ -11,7 +11,7 @@
 ///
 /// Do nothing
 
-pub async fn process_new_round_events(NewRoundEvent) {
+pub async fn process_new_round_event(NewRoundEvent) {
     let proposer_info = match self
         .proposer_election
         .is_valid_proposer(self.author, new_round_event.round)
@@ -124,6 +124,9 @@ pub async fn process_proposal(ProposalInfo<T, P>,) {
                     );
                     return;
                 }
+
+
+                // This calls process_winning_proposal
                 self.proposer_election.process_proposal(proposal).await;
             },
         }
@@ -195,7 +198,7 @@ async fn process_commit(
 /// 2. Try to vote for it following the safety rules.
 /// 3. In case a validator chooses to vote, send the vote to the representatives at the next
 /// position.
-async fn process_winning_proposals(ProposalInfo<T, P>,
+async fn process_winning_proposal(ProposalInfo<T, P>,
 ) {
     let qc = proposal.proposal.quorum_cert();
     let update_res = self.safety_rules.write().unwrap().update(qc);
@@ -616,3 +619,280 @@ pub fn voting_rule(
     }
 }
 // ----------------------------------------------------------------
+
+// Block store APIs -----------------------------------------------
+
+impl<T: Payload> BlockStore<T> {
+
+    pub fn signer(&self) -> &ValidatorSigner<Ed25519PrivateKey> {
+        &self.validator_signer
+    }
+
+    /// Execute and insert a block if it passes all validation tests.
+    /// Returns the Arc to the block kept in the block store after persisting it to storage
+    ///
+    /// This function assumes that the ancestors are present (returns MissingParent otherwise).
+    ///
+    /// Duplicate inserts will return the previously inserted block (
+    /// note that it is considered a valid non-error case, for example, it can happen if a validator
+    /// receives a certificate for a block that is currently being added).
+    pub async fn execute_and_insert_block(
+        &self,
+        block: Block<T>,
+    ) -> Result<Arc<Block<T>>, InsertError> {
+        if let Some(existing_block) = self.inner.read().unwrap().get_block(block.id()) {
+            return Ok(existing_block);
+        }
+        let (parent_id, parent_exec_version) = match self.verify_and_get_parent_info(&block) {
+            Ok(t) => t,
+            Err(e) => {
+                security_log(SecurityEvent::InvalidBlock)
+                    .error(&e)
+                    .data(&block)
+                    .log();
+                return Err(e);
+            }
+        };
+        let compute_res = self
+            .state_computer
+            .compute(parent_id, block.id(), block.get_payload())
+            .await
+            .map_err(|e| {
+                error!("Execution failure for block {}: {:?}", block, e);
+                InsertError::StateComputerError
+            })?;
+
+        let version = parent_exec_version + compute_res.num_successful_txns;
+
+        let state = ExecutedState {
+            state_id: compute_res.new_state_id,
+            version,
+        };
+        self.storage
+            .save_tree(vec![block.clone()], vec![])
+            .map_err(|_| InsertError::StorageFailure)?;
+        self.inner
+            .write()
+            .unwrap()
+            .insert_block(block, state, compute_res)
+            .map_err(|e| e.into())
+    }
+
+    /// Check if we're far away from this ledger info and need to sync.
+    /// Returns false if we have this block in the tree or the root's round is higher than the
+    /// block.
+    pub fn need_sync_for_quorum_cert(
+        &self,
+        committed_block_id: HashValue,
+        qc: &QuorumCert,
+    ) -> bool {
+        // LedgerInfo doesn't carry the information about the round of the committed block. However,
+        // the 3-chain safety rules specify that the round of the committed block must be
+        // certified_block_round() - 2. In case root().round() is greater than that the committed
+        // block carried by LI is older than my current commit.
+        !(self.block_exists(committed_block_id)
+            || self.root().round() + 2 >= qc.certified_block_round())
+    }
+
+    /// Checks if quorum certificate can be inserted in block store without RPC
+    /// Returns the enum to indicate the detailed status.
+    pub fn need_fetch_for_quorum_cert(&self, qc: &QuorumCert) -> NeedFetchResult {
+        if qc.certified_block_round() < self.root().round() {
+            return NeedFetchResult::QCRoundBeforeRoot;
+        }
+        if self
+            .get_quorum_cert_for_block(qc.certified_block_id())
+            .is_some()
+        {
+            return NeedFetchResult::QCAlreadyExist;
+        }
+        if self.block_exists(qc.certified_block_id()) {
+            return NeedFetchResult::QCBlockExist;
+        }
+        NeedFetchResult::NeedFetch
+    }
+
+    /// Validates quorum certificates and inserts it into block tree assuming dependencies exist.
+    pub async fn insert_single_quorum_cert(&self, qc: QuorumCert) -> Result<(), InsertError> {
+        // Ensure executed state is consistent with Quorum Cert, otherwise persist the quorum's
+        // state and hopefully we restart and agree with it.
+        let executed_state = self
+            .get_state_for_block(qc.certified_block_id())
+            .ok_or_else(|| InsertError::MissingParentBlock(qc.certified_block_id()))?;
+        assert_eq!(
+            executed_state,
+            qc.certified_state(),
+            "We have inconsistent executed state with the executed state from the quorum \
+             certificate for block {}, will kill this validator and rely on state synchronization \
+             to try to achieve consistent state with the quorum certificate.",
+            qc.certified_block_id(),
+        );
+        self.storage
+            .save_tree(vec![], vec![qc.clone()])
+            .map_err(|_| InsertError::StorageFailure)?;
+        self.inner
+            .write()
+            .unwrap()
+            .insert_quorum_cert(qc)
+            .map_err(|e| e.into())
+    }
+
+    /// Adds a vote for the block.
+    /// The returned value either contains the vote result (with new / old QC etc.) or a
+    /// verification error.
+    /// A block store does not verify that the block, which is voted for, is present locally.
+    /// It returns QC, if it is formed, but does not insert it into block store, because it might
+    /// not have required dependencies yet
+    /// Different execution ids are treated as different blocks (e.g., if some proposal is
+    /// executed in a non-deterministic fashion due to a bug, then the votes for execution result
+    /// A and the votes for execution result B are aggregated separately).
+    pub async fn insert_vote(
+        &self,
+        vote_msg: VoteMsg,
+        min_votes_for_qc: usize,
+    ) -> VoteReceptionResult {
+        self.inner
+            .write()
+            .unwrap()
+            .insert_vote(&vote_msg, min_votes_for_qc)
+    }
+
+    /// If block id information is found, returns the ledger info placeholder, otherwise, return
+    /// a placeholder with info of the genesis block.
+    pub fn ledger_info_placeholder(&self, id: Option<HashValue>) -> LedgerInfo {
+        let block_id = match id {
+            None => return Self::zero_ledger_info_placeholder(),
+            Some(id) => id,
+        };
+        let block = match self.get_block(block_id) {
+            Some(b) => b,
+            None => {
+                return Self::zero_ledger_info_placeholder();
+            }
+        };
+        let (state_id, version) = match self.get_state_for_block(block_id) {
+            Some(state) => (state.state_id, state.version),
+            None => {
+                return Self::zero_ledger_info_placeholder();
+            }
+        };
+        LedgerInfo::new(
+            version,
+            state_id,
+            HashValue::zero(),
+            block_id,
+            0, // TODO [Reconfiguration] use the real epoch number.
+            block.timestamp_usecs(),
+        )
+    }
+
+    /// Used in case we're using a ledger info just as a placeholder for signing the votes / QCs
+    /// and there is no real block committed.
+    /// It's all pretty much zeroes.
+    fn zero_ledger_info_placeholder() -> LedgerInfo {
+        LedgerInfo::new(
+            0,
+            HashValue::zero(),
+            HashValue::zero(),
+            HashValue::zero(),
+            0,
+            0,
+        )
+    }
+
+    fn verify_and_get_parent_info(
+        &self,
+        block: &Block<T>,
+    ) -> Result<(HashValue, u64), InsertError> {
+        if block.round() <= self.inner.read().unwrap().root().round() {
+            return Err(InsertError::OldBlock);
+        }
+
+        let block_hash = block.hash();
+        if block.id() != block_hash {
+            return Err(InsertError::InvalidBlockHash);
+        }
+
+        if block.quorum_cert().certified_block_id() != block.parent_id() {
+            return Err(InsertError::ParentNotCertified);
+        }
+
+        let parent = match self.inner.read().unwrap().get_block(block.parent_id()) {
+            None => {
+                return Err(InsertError::MissingParentBlock(block.parent_id()));
+            }
+            Some(parent) => parent,
+        };
+        if parent.height() + 1 != block.height() {
+            return Err(InsertError::InvalidBlockHeight);
+        }
+        if parent.round() >= block.round() {
+            return Err(InsertError::InvalidBlockRound);
+        }
+        if self.enforce_increasing_timestamps && parent.timestamp_usecs() >= block.timestamp_usecs()
+        {
+            return Err(InsertError::NonIncreasingTimestamp);
+        }
+        let parent_id = parent.id();
+        match self.inner.read().unwrap().get_state_for_block(parent_id) {
+            Some(ExecutedState { version, .. }) => Ok((parent.id(), version)),
+            None => Err(InsertError::ParentVersionNotFound),
+        }
+    }
+}
+
+impl<T: Payload> BlockReader for BlockStore<T> {
+    type Payload = T;
+
+    fn block_exists(&self, block_id: HashValue) -> bool {
+        self.inner.read().unwrap().block_exists(block_id)
+    }
+
+    fn get_block(&self, block_id: HashValue) -> Option<Arc<Block<Self::Payload>>> {
+        self.inner.read().unwrap().get_block(block_id)
+    }
+
+    fn get_state_for_block(&self, block_id: HashValue) -> Option<ExecutedState> {
+        self.inner.read().unwrap().get_state_for_block(block_id)
+    }
+
+    fn get_compute_result(&self, block_id: HashValue) -> Option<Arc<StateComputeResult>> {
+        self.inner.read().unwrap().get_compute_result(block_id)
+    }
+
+    fn root(&self) -> Arc<Block<Self::Payload>> {
+        self.inner.read().unwrap().root()
+    }
+
+    fn get_quorum_cert_for_block(&self, block_id: HashValue) -> Option<Arc<QuorumCert>> {
+        self.inner
+            .read()
+            .unwrap()
+            .get_quorum_cert_for_block(block_id)
+    }
+
+    fn is_ancestor(
+        &self,
+        ancestor: &Block<Self::Payload>,
+        block: &Block<Self::Payload>,
+    ) -> Result<bool, BlockTreeError> {
+        self.inner.read().unwrap().is_ancestor(ancestor, block)
+    }
+
+    fn path_from_root(&self, block: Arc<Block<T>>) -> Option<Vec<Arc<Block<T>>>> {
+        self.inner.read().unwrap().path_from_root(block)
+    }
+
+    fn highest_certified_block(&self) -> Arc<Block<Self::Payload>> {
+        self.inner.read().unwrap().highest_certified_block()
+    }
+
+    fn highest_quorum_cert(&self) -> Arc<QuorumCert> {
+        self.inner.read().unwrap().highest_quorum_cert()
+    }
+
+    fn highest_ledger_info(&self) -> Arc<QuorumCert> {
+        self.inner.read().unwrap().highest_ledger_info()
+    }
+}
+// ------------------------------------------
