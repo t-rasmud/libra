e7db156312243b551c5fad88043886df70eb4499
Rashmi Mudduluru <rashmimudduluru@calibra.com>
2019-08-15 16:32:37 -0700

    tmp commit

--- hotstuff_model/src/main.rs
+++ hotstuff_model/src/main.rs
@@ -5,29 +5,32 @@
 
 #![feature(async_await)]
 
-use mirai_annotations::{assume, assumed_postcondition, result};
-use rand::Rng;
+use mirai_annotations::{assumed_postcondition, result};
 
 static root: BlockId = 0;
 
-type BlockId = u64;
+type BlockId = usize;
 
 #[derive(Copy, Clone)]
 struct Block {
     parent: BlockId,
-    height: u64,
+    height: usize,
     justify: BlockId,
 }
-type ReplicaId = u64;
+type ReplicaId = usize;
 
 fn hash(bl: Block) -> BlockId {
     let res = result!();
     return res;
 }
 
+// global model variables [TODO: convert these to model fields]
+static mut voteStore: &'static mut [usize; 10000] = &mut [0; 10000];        // initially: lambda BlockId. f
+static mut committed:BlockId = 0;                                               // initially: root
+
 fn main() {
-    let f: u64 = result!();
-    let n: u64 = 2*f + 1;
+    let f: usize = result!();
+    let n: usize = 2*f + 1;
     let blocks: [Block; 10000] = result!();
 
     loop {
@@ -37,7 +40,7 @@ fn main() {
     }
 }
 
-fn havocReplica(maxReplicas: u64) -> ReplicaId {
+fn havocReplica(maxReplicas: usize) -> ReplicaId {
     let res = result!();
     assumed_postcondition!(res >=1 && res <= maxReplicas);
     return res;
@@ -47,6 +50,54 @@ fn havocBlockId() -> BlockId {
     result!()
 }
 
-async fn OnReceiveProposal(n: u64, f: u64, blocks: [Block; 10000], r: ReplicaId, newBlockId : BlockId) {
+// per-replica state
+static mut replicaStore: &'static mut [usize; 10000] = &mut [0; 10000];
+struct ReplicaState {
+    lockedBlockId: BlockId,         // initially root
+    vheight: usize,                  // initially 0
+    locallyCommitted: BlockId,      // initially: root
+}
 
+//per-replica code
+async fn OnReceiveProposal(n: usize, f: usize, blocks: [Block; 10000], r: ReplicaId, newBlockId : BlockId) {
+    let newBlock: Block;
+    newBlock = blocks[newBlockId];
+
+    unsafe{
+        if voteStore[newBlock.justify] < n {
+            return
+        }
+    }
+    if newBlock.height > vheight[r] &&
+        (extends(newBlockId, lockedBlockId[r]) ||
+            blocks[newBlock.justify].height > blocks[lockedBlockId[r]].height) {
+        vheight[r] = newBlock.height;
+
+        unsafe{
+            voteStore[newBlockId] = voteStore[newBlockId] + 1;
+        }
+  }
+  Update(blocks, r, newBlock.justify)
+}
+
+fn Update(blocks: [Block; 10000], r: ReplicaId, id_double_prime: BlockId) {
+    let b_double_prime = blocks[id_double_prime];
+    let id_prime = b_double_prime.justify;
+    let b_prime = blocks[id_prime];
+    let id = b_prime.justify;
+    let b = blocks[id];
+    if b_prime.height > blocks[lockedBlockId[r]].height {
+        lockedBlockId[r] = id_prime;
+    }
+    if b_double_prime.parent == hash(b_prime) && b_prime.parent == hash(b) {
+        assert!(extends(b, locallyCommitted[r]));
+        locallyCommitted[r] = b;
+        unsafe{
+            if reaches(b, committed) {
+                committed = hash(b);
+            } else {
+                assert!(reaches(committed, b));
+            }
+        }
+    }
 }
