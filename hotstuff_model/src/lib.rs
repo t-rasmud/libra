38e43aee6e87a443357c7a36ce7134f61ec4050e
Rashmi Mudduluru <rashmimudduluru@calibra.com>
2019-08-19 12:08:04 -0700

    create vote store inside main, remove unsafe

--- hotstuff_model/src/lib.rs
+++ hotstuff_model/src/lib.rs
@@ -12,29 +12,21 @@ type BlockId = usize;
 struct Block {
     parent: BlockId,
     height: usize,
+    // BlockId for which QC has been gathered.
     justify: BlockId,
 }
 
+// Returns a random number between 0 and NUM_BLOCKS.
+// axiom forall id: BlockId. Hash(blocks[id]) == id
+// checked at use site.
 fn hash(_bl: Block) -> BlockId {
     let mut rng = rand::thread_rng();
     let res:usize = rng.gen_range(0, NUM_BLOCKS);
     return res;
 }
 
-static ROOT: BlockId = 0;               // root of block tree
+const ROOT: BlockId = 0;               // root of block tree
 type HonestReplicaId = usize;
-
-
-/// global model variables [TODO: convert these to model fields?]
-// all collected votes
-// initial value: lambda id: BlockId. if id == root then h+f else f (maximum equivocation)
-static mut VOTE_STORE: &'static mut [usize; NUM_BLOCKS] = &mut [0; NUM_BLOCKS];
-
-//// Model structs
-//struct voteMap {
-//    blockId: BlockId,
-//}
-//static  modelVoteStore: [usize; numBlocks] =  [0; numBlocks];
 
 // id of the latest globally-committed block
 // initial value: root
@@ -59,7 +51,6 @@ struct ReplicaState {
 // h: number of honest replicas
 // blocks: constant set of blocks
 fn main_method(f: usize, h: usize, blocks: [Block; NUM_BLOCKS]) {
-
     //axiom h >= 2f + 1
     assume!(h >= 2*f + 1);
     //axiom blocks[root].height == 0
@@ -68,18 +59,19 @@ fn main_method(f: usize, h: usize, blocks: [Block; NUM_BLOCKS]) {
     // NUM_REPLICAS = h + f
     assume!(NUM_REPLICAS == h + f);
 
-    // initial value: lambda id: BlockId. if id == root then h+f else f (maximum equivocation)
-    unsafe{
-        VOTE_STORE[ROOT] = h + f;
-        for i in 1..NUM_BLOCKS {
-            VOTE_STORE[i] = f;
-        }
-    }
-
 //    set_model_field!(&modelVoteStore[root], numVotes, h + f);
 //    for i in 1..numBlocks {
 //        set_model_field!(&modelVoteStore[i], numVotes, f);
 //    }
+
+    /// global model variables [TODO: convert these to model fields?]
+    // all collected votes
+    // initial value: lambda id: BlockId. if id == root then h+f else f (maximum equivocation)
+    let vote_store: & mut [usize; NUM_BLOCKS] = &mut [0; NUM_BLOCKS];
+    vote_store[ROOT] = h + f;
+    for i in 1..NUM_BLOCKS {
+        vote_store[i] = f;
+    }
 
     let replica_store: & mut [ReplicaState; NUM_REPLICAS] =
         &mut [ReplicaState{vheight: 0, locked_block_id: ROOT, locally_committed: ROOT }; NUM_REPLICAS];
@@ -87,7 +79,7 @@ fn main_method(f: usize, h: usize, blocks: [Block; NUM_BLOCKS]) {
 
     let r = havoc_replica(h);
     let new_block_id = havoc_block_id();
-    on_receive_proposal(h, f, blocks, r, replica_store, new_block_id);
+    on_receive_proposal(h, f, blocks, r, replica_store, new_block_id, vote_store);
 }
 
 fn havoc_replica(max_replicas: usize) -> HonestReplicaId {
@@ -102,15 +94,18 @@ fn havoc_block_id() -> BlockId {
     return res;
 }
 
-async fn async_main(f: usize, h: usize, blocks: [Block; NUM_BLOCKS], replica_store: & mut [ReplicaState; NUM_REPLICAS]) {
+async fn async_main(f: usize, h: usize, blocks: [Block; NUM_BLOCKS],
+                    replica_store: & mut [ReplicaState; NUM_REPLICAS],
+                    vote_store: & mut [usize; NUM_BLOCKS]) {
     let r = havoc_replica(h);
     let new_block_id = havoc_block_id();
-    on_receive_proposal(h, f, blocks, r, replica_store, new_block_id).await;
+    on_receive_proposal(h, f, blocks, r, replica_store, new_block_id, vote_store).await;
 }
 
 /// top-level event handler at a replica to update vheight and "send" vote
 async fn on_receive_proposal(h: usize, f: usize, blocks: [Block; NUM_BLOCKS], r: HonestReplicaId,
-                           replica_store: & mut [ReplicaState; NUM_REPLICAS], new_block_id : BlockId) {
+                           replica_store: & mut [ReplicaState; NUM_REPLICAS], new_block_id : BlockId,
+                             vote_store: & mut [usize; NUM_BLOCKS]) {
     let new_block: Block;
     new_block = blocks[new_block_id];
 
@@ -120,18 +115,17 @@ async fn on_receive_proposal(h: usize, f: usize, blocks: [Block; NUM_BLOCKS], r:
     let mut rng = rand::thread_rng();
     let havoc_bool:bool = rng.gen();
 
-    unsafe{
-        if havoc_bool && VOTE_STORE[new_block.justify] >= h {
-            if new_block.height > replica_store[r].vheight &&
-                (extends(blocks, new_block_id, replica_store[r].locked_block_id) ||
-                    blocks[new_block.justify].height > blocks[replica_store[r].locked_block_id].height) {
-                replica_store[r].vheight = new_block.height;
-                VOTE_STORE[new_block_id] = VOTE_STORE[new_block_id] + 1;
-            }
+    if havoc_bool && vote_store[new_block.justify] >= h {
+        if new_block.height > replica_store[r].vheight &&
+            (extends(blocks, new_block_id, replica_store[r].locked_block_id) ||
+                blocks[new_block.justify].height > blocks[replica_store[r].locked_block_id].height) {
+            replica_store[r].vheight = new_block.height;
+            vote_store[new_block_id] = vote_store[new_block_id] + 1;
         }
     }
+
     async_update(blocks, r, replica_store, new_block.justify).await;
-    async_main(f, h, blocks, replica_store);
+    async_main(f, h, blocks, replica_store, vote_store);
 }
 
 /// Internal event handler at a replica to update lockedBlockId, locallyCommitted, and committed
